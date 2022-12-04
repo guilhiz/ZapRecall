018d9977cd77c5914e5fb32245374c17ba215849
guilhiz <guilhermevilelasobral@gmail.com>
2022-12-03 21:07:30 -0300

    feat: add data-tests

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
 import play from "../../img/seta_play.png";
+import miss from "../../img/icone_erro.png";
+import zap from "../../img/icone_certo.png";
+import partial from "../../img/icone_quase.png";
 import turn from "../../img/seta_virar.png";
 import { ClosedQuestion, OpenQuestion, ContainerBotoes, AnswerButton } from "./style";
 
@@ -9,6 +12,8 @@ export default function Card(props) {
   const [activeSwitch, setActiveSwitch] = useState(true);
   const [textColor, SetTextColor] = useState("#333333");
   const [textDecoration, setTextDecoration] = useState("none");
+  const [icon, setIcon] = useState(play)
+  const [dataTest, setDataTest] = useState("play-btn")
 
   function handleClick(props) {
     setCardSwitch("closed");
@@ -17,39 +22,45 @@ export default function Card(props) {
     setAnswerCounter(answerCounter + 1);
     if (props === "red") {
       SetTextColor("#FF3030");
+      setIcon(miss)
+      setDataTest("no-icon")
     } else if (props === "orange") {
       SetTextColor("#FF922E");
+      setIcon(partial)
+      setDataTest("partial-icon")
     } else if (props === "green") {
       SetTextColor("#2FBE34");
+      setIcon(zap)
+      setDataTest("zap-icon")
     }
   }
 
   if (cardSwitch === "closed") {
     return (
-      <ClosedQuestion textColor={textColor} textDecoration={textDecoration}>
-        <p>{`pergunta ${i + 1}`}</p>
-        <img onClick={() => activeSwitch && setCardSwitch("openQuestion")} src={play} alt="icon de play" />
+      <ClosedQuestion data-test="flashcard" textColor={textColor} textDecoration={textDecoration}>
+        <p data-test="flashcard-text">{`pergunta ${i + 1}`}</p>
+        <img data-test={dataTest}  onClick={() => activeSwitch && setCardSwitch("openQuestion")} src={icon} alt="icon de play" />
       </ClosedQuestion>
     );
   } else if (cardSwitch === "openQuestion") {
     return (
-      <OpenQuestion>
-        <p>{question}</p>
-        <img onClick={() => setCardSwitch("openAnswer")} src={turn} alt="icon de turn" />
+      <OpenQuestion data-test="flashcard">
+        <p data-test="flashcard-text">{question}</p>
+        <img data-test="turn-btn" onClick={() => setCardSwitch("openAnswer")} src={turn} alt="icon de turn" />
       </OpenQuestion>
     );
   }
   return (
-    <OpenQuestion>
-      <p>{answer}</p>
+    <OpenQuestion data-test="flashcard">
+      <p data-test="flashcard-text">{answer}</p>
       <ContainerBotoes>
-        <AnswerButton onClick={() => handleClick("red")} color="#FF3030">
+        <AnswerButton  data-test="no-btn" onClick={() => handleClick("red")} color="#FF3030">
           Não lembrei
         </AnswerButton>
-        <AnswerButton onClick={() => handleClick("orange")} color="#FF922E">
+        <AnswerButton data-test="partial-btn" onClick={() => handleClick("orange")} color="#FF922E">
           Quase não lembrei
         </AnswerButton>
-        <AnswerButton onClick={() => handleClick("green")} color="#2FBE34">
+        <AnswerButton data-test="zap-btn" onClick={() => handleClick("green")} color="#2FBE34">
           Zap
         </AnswerButton>
       </ContainerBotoes>
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import { FooterCompleted } from "./styles";
 export default function Footer(props) {
   const { answerCounter } = props;
   return (
-    <FooterCompleted className="footer-concluidos">
+    <FooterCompleted data-test="footer" className="footer-concluidos">
       <p>{answerCounter}/8 CONCLUÍDOS</p>
     </FooterCompleted>
   );
